259f3729e95e00d4d38df5efff0c942cc68e8b9c
jmetz2131 <jacob.metz@wustl.edu>
2016-02-08 19:59:15 -0600

    Setting Up the Input to Allow for a Matrix and a Vector
    As I specified in my comments, I am trying to get my Benford’s Law
    function to take a hypothetical example of a random sample of vote
    counts across three candidates in 20 counties. For the actual function
    itself, I want it to accept the proportions of the first digits,
    meaning that if the first digit “1” occurs 30% of the time, I want the
    function to reflect that by inputting .3. I attempted to do that with
    the proportion_matrix, but I am thinking that I will take that part
    out. I’m doing my best to figure this out and I might simplify the
    hypothetical example going forward to make the code simpler.

--- PS2.R
+++ PS2.R
@@ -2,18 +2,35 @@
 ####Homework #2
 ###Question 1
 ##Leemis' m statistic and Cho-Gains' d
-vote.results <- seq(0,1, by = .01)
-vote.results
+vote.results <- seq(0,10000, by = 1) # Here, I used a sequence of 0 to 10000 to simulate the amount of votes a candidate would receive in a single district.
 
-election.vector <- sample(vote.results, 9)
-election.vector
-election.matrix
+candidate.a <- sample(vote.results, 20, replace=T)
+candidate.b <- sample(vote.results, 20, replace=T)
+candidate.c <- sample(vote.results, 20, replace=T)
+#For the three lines of code above, I made the vectors candidate.a, candidate.b, and candidate.c to represent how many votes three candidates would receive in a hypothetical election. 
+#I then took a sample of vote.results 20 times to represent the vote count totals for 20 different counties to truly test if Benford's Law works out for a random sample.
 
-benford = function(...){
-  i <- 1:9
-  m <- max(x-log(1+1/i, base=10))
-  d <- sqrt((x-log(1+1/i, base=10))^2)
+county_results <- cbind(candidate.a, candidate.b, candidate.c)
+county_results
+#The matrix county_results includes the vote count totals for candidate.a, candidate.b, and candidate.c across twenty different hypothetical counties.
+
+first_digit <- substr(county_results, start=1, stop=1)
+x <- as.numeric(first_digit)
+#I then took the county_results matrix and used the substr() function to take just the first digit
+#to see the distribution of digits across the vote totals to see if this example satisfies Benford's Law
+
+proportion_matrix = function(x){
+  first_digit[first_digit==x]
 }
+function(x)
+
+
+benford = function(x){
+  i <- 1:9
+  m <- max((x*100/sum(x))-log(1+1/i, base=10))
+  d <- sqrt(sum((x*100/sum(x))-log(1+1/i, base=10))^2)
+}
+benford(x)
 
 ###Question 2
 print.benford = function()
